cbeeae5767ca954a9e9297a5e8921a87edc47715
Eren0414 <kritof.jager.2006@gmail.com>
2024-01-23 11:58:47 +0100

    teszt

---
+++ ExpedicioProjekt/ExpedicioProjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpedicioProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
